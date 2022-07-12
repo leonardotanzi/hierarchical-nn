764d9216d9c6e5a5fc2ac22a6971d596f57da067
leonardotanzi <leo.tanzi@gmail.com>
2022-07-12 15:21:57 +0200

    32

--- Code/losses.py
+++ Code/losses.py
@@ -17,7 +17,7 @@ def hierarchical_cc_treebased(predicted, actual, tree, lens, all_labels, all_lea
 
     batch = predicted.size(0)
 
-    predicted = torch.softmax(predicted, dim=1) + 1e-6
+    predicted = torch.softmax(predicted, dim=1) + 1e-12
     loss = cross_entropy(predicted, actual, reduction="sum")
 
     loss_dict = {"loss_fine": loss.item()}
@@ -29,6 +29,9 @@ def hierarchical_cc_treebased(predicted, actual, tree, lens, all_labels, all_lea
             labels_coarser = sparser2coarser(actual, torch.tensor(labels, requires_grad=False).type(torch.int64).to(device))
             predicted_coarser = torch.matmul(predicted, torch.tensor(matrixes[i], requires_grad=False).type(torch.float32).to(device))
             loss_coarser = cross_entropy(predicted_coarser, labels_coarser, reduction="sum")
+
+            if loss_coarser < 0:
+                stop = True
 
             loss_dict[f"loss_{i}"] = loss_coarser.item()
             loss_hierarchical += loss_coarser
