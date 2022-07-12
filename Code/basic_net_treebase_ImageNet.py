9e372f21c6942754359c295bf6005277bda786ba
leonardotanzi <leo.tanzi@gmail.com>
2022-07-12 10:11:11 +0200

    128

--- Code/basic_net_treebase_ImageNet.py
+++ Code/basic_net_treebase_ImageNet.py
@@ -34,8 +34,8 @@ if __name__ == "__main__":
     scheduler_step_size = 40
     validation_split = 0.1
 
-    hierarchical_loss = False
-    regularization = False
+    hierarchical_loss = True
+    regularization = True
     name = "resnet-imagenet-doublemat-unfreezed"
 
     run_scheduler = False
