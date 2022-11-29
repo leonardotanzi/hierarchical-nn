cc68a0cacac42e53616865a55032bdf5c8faa64a
leonardotanzi <leo.tanzi@gmail.com>
2022-11-30 00:33:41 +0100

    tddt

--- Code/train.py
+++ Code/train.py
@@ -38,7 +38,7 @@ if __name__ == "__main__":
     dict_architectures = {"inception": [299, 256], "resnet": [224, 256], "vit": [224, 128]}
 
     image_size = dict_architectures[architecture][0]
-    batch_size = dict_architectures[architecture][1]
+    batch_size = 64 #dict_architectures[architecture][1]
     n_epochs = 20
     learning_rate = 0.001
     scheduler_step_size = 40
@@ -46,11 +46,11 @@ if __name__ == "__main__":
 
     hierarchical_loss = (args["hloss"] == "True")
     regularization = (args["hloss"] == "True")
-    name = f"{architecture}-{dataset}"
+
 
     run_scheduler = False
     sp_regularization = False
-    weight_decays = [10, 1, 0.1, 0.01, 0.001, 0.0001]
+    weight_decay = 0.1
     less_samples = True
     reduction_factor = int(args["reduction"])
     freeze = False
@@ -64,13 +64,28 @@ if __name__ == "__main__":
     all_nodes_names = [[node.name for node in children] for children in LevelOrderGroupIter(tree)][1:]
     all_nodes = [[node for node in children] for children in LevelOrderGroupIter(tree)][1:]
 
-    all_labels_topdown = get_all_labels_topdown(tree)
-    all_labels_downtop = get_all_labels_downtop(tree)
-    all_labels = [*all_labels_topdown, *all_labels_downtop]
+    name = f"{architecture}-{dataset}-both"
+    only_td = False
+    only_dt = False
 
-    matrixes_topdown = return_matrixes_topdown(tree, plot=False)
-    matrixes_downtop = return_matrixes_downtop(tree, plot=False)
-    matrixes = [*matrixes_topdown, *matrixes_downtop]
+    if only_td:
+        all_labels_topdown = get_all_labels_topdown(tree)
+        all_labels = all_labels_topdown
+        matrixes_topdown = return_matrixes_topdown(tree, plot=False)
+        matrixes = matrixes_topdown
+    elif only_dt:
+        all_labels_downtop = get_all_labels_downtop(tree)
+        all_labels = all_labels_downtop
+        matrixes_downtop = return_matrixes_downtop(tree, plot=False)
+        matrixes = matrixes_downtop
+    else:
+        all_labels_topdown = get_all_labels_topdown(tree)
+        all_labels_downtop = get_all_labels_downtop(tree)
+        all_labels = [*all_labels_topdown, *all_labels_downtop]
+
+        matrixes_topdown = return_matrixes_topdown(tree, plot=False)
+        matrixes_downtop = return_matrixes_downtop(tree, plot=False)
+        matrixes = [*matrixes_topdown, *matrixes_downtop]
 
     lens = [len(set(n)) for n in all_labels]
 
@@ -84,7 +99,7 @@ if __name__ == "__main__":
 
     # Load the data: train and test sets
     train_dataset = ImageFolderNotAlphabetic(train_dir, classes=all_leaves, transform=transform)
-    dataset = train_val_dataset(train_dataset, validation_split, reduction_factor, reduce_val=True)
+    dataset = train_val_dataset(train_dataset, validation_split, reduction_factor, reduce_val=False)
 
     train_loader = DataLoader(dataset["train"], batch_size=batch_size, shuffle=True, drop_last=True, num_workers=4)
     val_loader = DataLoader(dataset["val"], batch_size=batch_size, shuffle=False, drop_last=True, num_workers=4)
@@ -128,162 +143,159 @@ if __name__ == "__main__":
         scheduler = lr_scheduler.StepLR(optimizer, step_size=scheduler_step_size, gamma=0.3)
         # every n=step_size epoch the lr is multiplied by gamma
 
-    for weight_decay in weight_decays:
-
-        # Optimizer
-        optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate) if regularization \
+    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate) if regularization \
             else torch.optim.Adam(model.parameters(), lr=learning_rate, weight_decay=weight_decay)
 
-        # Path
-        model_path = f"..//..//Models//WD//"  # {architecture}-{dataset}//"
+    # Path
+    model_path = f"..//..//Models//td-dt//"  # {architecture}-{dataset}//"
 
-        print(f"Training with WD {weight_decay}")
-        if hierarchical_loss and not regularization:
-            model_name = os.path.join(model_path,
-                                      f"{name}_hloss_lr{decimal_to_string(learning_rate)}_wd{decimal_to_string(weight_decay)}_1on{reduction_factor}.pth")
-        elif regularization and not hierarchical_loss:
-            model_name = os.path.join(model_path,
-                                      f"{name}_reg_lr{decimal_to_string(learning_rate)}_wd{decimal_to_string(weight_decay)}_1on{reduction_factor}.pth")
-        elif regularization and hierarchical_loss:
-            model_name = os.path.join(model_path,
-                                      f"{name}_hloss_reg_lr{decimal_to_string(learning_rate)}_wd{decimal_to_string(weight_decay)}_1on{reduction_factor}.pth")
-        else:
-            model_name = os.path.join(model_path,
-                                      f"{name}_lr{decimal_to_string(learning_rate)}_wd{decimal_to_string(weight_decay)}_1on{reduction_factor}.pth")
-        print(f"Model name: {model_name}")
+    print(f"Training with WD {weight_decay}")
+    if hierarchical_loss and not regularization:
+        model_name = os.path.join(model_path,
+                                  f"{name}_hloss_lr{decimal_to_string(learning_rate)}_wd{decimal_to_string(weight_decay)}_1on{reduction_factor}.pth")
+    elif regularization and not hierarchical_loss:
+        model_name = os.path.join(model_path,
+                                  f"{name}_reg_lr{decimal_to_string(learning_rate)}_wd{decimal_to_string(weight_decay)}_1on{reduction_factor}.pth")
+    elif regularization and hierarchical_loss:
+        model_name = os.path.join(model_path,
+                                  f"{name}_hloss_reg_lr{decimal_to_string(learning_rate)}_wd{decimal_to_string(weight_decay)}_1on{reduction_factor}.pth")
+    else:
+        model_name = os.path.join(model_path,
+                                  f"{name}_lr{decimal_to_string(learning_rate)}_wd{decimal_to_string(weight_decay)}_1on{reduction_factor}.pth")
+    print(f"Model name: {model_name}")
 
-        # Parameters for training
-        n_total_steps_train = len(train_loader)
-        n_total_steps_val = len(val_loader)
-        platoon = 0
-        best_acc = 0.0
-        associated_medium_acc = 0.0
-        associated_coarse_acc = 0.0
+    # Parameters for training
+    n_total_steps_train = len(train_loader)
+    n_total_steps_val = len(val_loader)
+    platoon = 0
+    best_acc = 0.0
+    associated_medium_acc = 0.0
+    associated_coarse_acc = 0.0
 
-        for epoch in range(n_epochs):
-            start = timeit.default_timer()
-            print("-" * 200)
-            print(f"Epoch {epoch + 1}/{n_epochs}")
+    for epoch in range(n_epochs):
+        start = timeit.default_timer()
+        print("-" * 200)
+        print(f"Epoch {epoch + 1}/{n_epochs}")
 
-            for phase in ["train", "val"]:
-                if phase == "train":
-                    model.train()
-                    loader = train_loader
-                    n_total_steps = n_total_steps_train
-                else:
-                    model.eval()
-                    loader = val_loader
-                    n_total_steps = n_total_steps_val
+        for phase in ["train", "val"]:
+            if phase == "train":
+                model.train()
+                loader = train_loader
+                n_total_steps = n_total_steps_train
+            else:
+                model.eval()
+                loader = val_loader
+                n_total_steps = n_total_steps_val
 
-                running_loss = 0.0
-                running_corrects = 0
-                running_corrects_coarser_level = [0 for i in range(len(all_labels))]
-                running_loss_fine = 0.0
-                running_loss_coarser_level = [0.0 for i in range(len(all_labels))]
+            running_loss = 0.0
+            running_corrects = 0
+            running_corrects_coarser_level = [0 for i in range(len(all_labels))]
+            running_loss_fine = 0.0
+            running_loss_coarser_level = [0.0 for i in range(len(all_labels))]
 
-                epoch_acc_coarser = [0 for i in range(len(all_labels))]
-                epoch_loss_coarser = [0.0 for i in range(len(all_labels))]
+            epoch_acc_coarser = [0 for i in range(len(all_labels))]
+            epoch_loss_coarser = [0.0 for i in range(len(all_labels))]
 
-                # Iterate over data
-                for j, (inputs, labels) in enumerate(loader):
-                    inputs = inputs.to(device)
-                    labels = labels.to(device)
+            # Iterate over data
+            for j, (inputs, labels) in enumerate(loader):
+                inputs = inputs.to(device)
+                labels = labels.to(device)
 
-                    print(f"Step {j} / {len(loader)}")
+                print(f"Step {j} / {len(loader)}")
 
-                    # Forward
-                    with torch.set_grad_enabled(phase == "train"):
+                # Forward
+                with torch.set_grad_enabled(phase == "train"):
 
-                        outputs = model(inputs).logits if architecture == "vit" else model(inputs)
+                    outputs = model(inputs).logits if architecture == "vit" else model(inputs)
 
-                        _, preds = torch.max(outputs, 1)
+                    _, preds = torch.max(outputs, 1)
 
-                        loss, loss_dict = hierarchical_cc_treebased(outputs, labels, tree, lens, all_labels, all_leaves,
-                                                                    model, 0.0, device, hierarchical_loss, regularization,
-                                                                    sp_regularization, weight_decay, matrixes, architecture,
-                                                                    multigpu)
+                    loss, loss_dict = hierarchical_cc_treebased(outputs, labels, tree, lens, all_labels, all_leaves,
+                                                                model, 0.0, device, hierarchical_loss, regularization,
+                                                                sp_regularization, weight_decay, matrixes, architecture,
+                                                                multigpu)
 
-                        # Backward + optimize
-                        if phase == "train":
-                            optimizer.zero_grad()
-                            loss.backward()
-                            optimizer.step()
+                    # Backward + optimize
+                    if phase == "train":
+                        optimizer.zero_grad()
+                        loss.backward()
+                        optimizer.step()
 
-                    # Statistics
-                    running_loss += loss.item()
-                    running_corrects += torch.sum(preds == labels.data).item()
-                    running_loss_fine += loss_dict["loss_fine"]
+                # Statistics
+                running_loss += loss.item()
+                running_corrects += torch.sum(preds == labels.data).item()
+                running_loss_fine += loss_dict["loss_fine"]
+
+                for i in range(len(all_labels)):
+                    running_corrects_coarser_level[i] += accuracy_coarser_classes(outputs, labels, np.asarray(all_labels[i]),
+                                                                   len(all_labels[i]), device)
+                    running_loss_coarser_level[i] += loss_dict[f"loss_{i}"]
+
+            epoch_loss = running_loss / dataset_sizes[phase]
+            epoch_acc = running_corrects / dataset_sizes[phase]
+            epoch_loss_fine = running_loss_fine / dataset_sizes[phase]
+
+            for i in range(len(all_labels)):
+                epoch_acc_coarser[i] = running_corrects_coarser_level[i] / dataset_sizes[phase]
+                epoch_loss_coarser[i] = running_loss_coarser_level[i] / dataset_sizes[phase]
+
+            print(f"Step {j + 1}/{n_total_steps}, {phase} Loss: {epoch_loss:.4f}, Acc: {epoch_acc:.4f}")
+            for i in range(len(all_labels)):
+                print(f"{phase} Loss {i}: {epoch_loss_coarser[i]:.4f}, Accuracy {i}: {epoch_acc_coarser[i]:.4f}")
+
+            if (j + 1) % n_total_steps == 0:
+                if phase == "val":
+                    if epoch_acc > best_acc:
+                        best_acc = epoch_acc
+                        platoon = 0
+                        best_model_name = model_name[:-4] + "_best.pth"
+                        torch.save(model.state_dict(), best_model_name)
+                        print(f"New best accuracy {best_acc:.4f}, saving best model")
+
+                    if epoch_acc < best_acc:
+                        platoon += 1
+                        print(f"{platoon} epochs without improvement, best accuracy: {best_acc:.4f}")
+
+                    print("End of validation epoch.")
+                    writer.add_scalar("Validation loss", epoch_loss, epoch)
+                    writer.add_scalar("Validation accuracy", epoch_acc, epoch)
+                    for i in range(len(all_labels)):
+                        writer.add_scalar(f"Validation accuracy {i}", epoch_acc_coarser[i], epoch)
+
+                    writer.add_scalars("Training vs. validation loss",
+                                       {"Training": epoch_loss_compare, "Validation": epoch_loss}, epoch)
+                    writer.add_scalars("Training vs. validation accuracy",
+                                       {"Training": epoch_acc_compare, "Validation": epoch_acc}, epoch)
+
+                    plot_dict = {"Loss": epoch_loss, "Fine Loss": epoch_loss_fine}
 
                     for i in range(len(all_labels)):
-                        running_corrects_coarser_level[i] += accuracy_coarser_classes(outputs, labels, np.asarray(all_labels[i]),
-                                                                       len(all_labels[i]), device)
-                        running_loss_coarser_level[i] += loss_dict[f"loss_{i}"]
+                        plot_dict[f"Loss {i}"] = epoch_loss_coarser[i]
 
-                epoch_loss = running_loss / dataset_sizes[phase]
-                epoch_acc = running_corrects / dataset_sizes[phase]
-                epoch_loss_fine = running_loss_fine / dataset_sizes[phase]
+                    writer.add_scalars("Losses and penalties validation", plot_dict, epoch)
 
-                for i in range(len(all_labels)):
-                    epoch_acc_coarser[i] = running_corrects_coarser_level[i] / dataset_sizes[phase]
-                    epoch_loss_coarser[i] = running_loss_coarser_level[i] / dataset_sizes[phase]
+                elif phase == "train":
+                    if run_scheduler:
+                        scheduler.step()
+                    print("End of training epoch.")
+                    writer.add_scalar("Training loss", epoch_loss, epoch)
+                    writer.add_scalar("Training accuracy", epoch_acc, epoch)
+                    for i in range(len(all_labels)):
+                        writer.add_scalar(f"Training accuracy {i}", epoch_acc_coarser[i], epoch)
 
-                print(f"Step {j + 1}/{n_total_steps}, {phase} Loss: {epoch_loss:.4f}, Acc: {epoch_acc:.4f}")
-                for i in range(len(all_labels)):
-                    print(f"{phase} Loss {i}: {epoch_loss_coarser[i]:.4f}, Accuracy {i}: {epoch_acc_coarser[i]:.4f}")
+                    plot_dict = {"Loss": epoch_loss, "Fine Loss": epoch_loss_fine}
 
-                if (j + 1) % n_total_steps == 0:
-                    if phase == "val":
-                        if epoch_acc > best_acc:
-                            best_acc = epoch_acc
-                            platoon = 0
-                            best_model_name = model_name[:-4] + "_best.pth"
-                            torch.save(model.state_dict(), best_model_name)
-                            print(f"New best accuracy {best_acc:.4f}, saving best model")
+                    for i in range(len(all_labels)):
+                        plot_dict[f"Loss {i}"] = epoch_loss_coarser[i]
 
-                        if epoch_acc < best_acc:
-                            platoon += 1
-                            print(f"{platoon} epochs without improvement, best accuracy: {best_acc:.4f}")
+                    writer.add_scalars("Losses and penalties training", plot_dict, epoch)
+                    epoch_loss_compare = epoch_loss
+                    epoch_acc_compare = epoch_acc
 
-                        print("End of validation epoch.")
-                        writer.add_scalar("Validation loss", epoch_loss, epoch)
-                        writer.add_scalar("Validation accuracy", epoch_acc, epoch)
-                        for i in range(len(all_labels)):
-                            writer.add_scalar(f"Validation accuracy {i}", epoch_acc_coarser[i], epoch)
+        stop = timeit.default_timer()
+        print(f"Elapsed time {stop-start:.4f}")
 
-                        writer.add_scalars("Training vs. validation loss",
-                                           {"Training": epoch_loss_compare, "Validation": epoch_loss}, epoch)
-                        writer.add_scalars("Training vs. validation accuracy",
-                                           {"Training": epoch_acc_compare, "Validation": epoch_acc}, epoch)
+    last_model_name = model_name[:-4] + "_last.pth"
 
-                        plot_dict = {"Loss": epoch_loss, "Fine Loss": epoch_loss_fine}
-
-                        for i in range(len(all_labels)):
-                            plot_dict[f"Loss {i}"] = epoch_loss_coarser[i]
-
-                        writer.add_scalars("Losses and penalties validation", plot_dict, epoch)
-
-                    elif phase == "train":
-                        if run_scheduler:
-                            scheduler.step()
-                        print("End of training epoch.")
-                        writer.add_scalar("Training loss", epoch_loss, epoch)
-                        writer.add_scalar("Training accuracy", epoch_acc, epoch)
-                        for i in range(len(all_labels)):
-                            writer.add_scalar(f"Training accuracy {i}", epoch_acc_coarser[i], epoch)
-
-                        plot_dict = {"Loss": epoch_loss, "Fine Loss": epoch_loss_fine}
-
-                        for i in range(len(all_labels)):
-                            plot_dict[f"Loss {i}"] = epoch_loss_coarser[i]
-
-                        writer.add_scalars("Losses and penalties training", plot_dict, epoch)
-                        epoch_loss_compare = epoch_loss
-                        epoch_acc_compare = epoch_acc
-
-            stop = timeit.default_timer()
-            print(f"Elapsed time {stop-start:.4f}")
-
-        last_model_name = model_name[:-4] + "_last.pth"
-
-        writer.flush()
-        writer.close()
+    writer.flush()
+    writer.close()
